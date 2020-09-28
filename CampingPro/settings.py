d754f46177518d1e5362f8a3ee63f4546b15c510
kevin <kevin.reust@stud.hslu.ch>
2020-09-28 16:45:50 +0200

    Added new packets to requirements.txt and optimized import

--- CampingPro/settings.py
+++ CampingPro/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'your_key_here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
