2deb4d5d47f15bda104d166dbbd31620339f53df
kevin <kevin.reust@stud.hslu.ch>
2020-09-29 13:34:50 +0200

    Init Commit

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CampingPro.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'campingpro.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
